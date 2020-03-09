deb0360d8d88b0120fce72805517e5c67f94156d
Khalil Crumpler <crumplerkhalil@gmail.com>
2020-03-08 21:15:58 -0400

    finished lab

--- src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
@@ -43,19 +43,26 @@ public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer> {
     @Override
     public Integer[] removeDuplicatesExactly(int exactNumberOfDuplications) {
         int count = 0;
-        Integer[] newArray = new Integer[this.array.length];
-        for(int i = 0; i < this.array.length; i++){
-            int arrayCount = 0;
-            for(int j = 1; j < this.array.length; j++){
-                if(this.array[i].equals(this.array[j])){
-                    arrayCount += 1;
-                }
-            }
-            if(arrayCount > 0 && arrayCount <= exactNumberOfDuplications){
-                newArray[i] = this.array[i];
+        int secondCount = 0;
+        Integer[] tempArray = new Integer[0];
+
+        for(int i = 0; i < array.length; i++){
+            if(getOccurances(array[i]) != exactNumberOfDuplications){
+                tempArray = new Integer[tempArray.length + 1];
+                tempArray[count] = array[i];
+                count++;
             }
         }
-
+        Integer[] newArray = new Integer[tempArray.length];
+        for(int i = 0; i < array.length; i++){
+            if(getOccurances(array[i]) != exactNumberOfDuplications){
+                newArray[secondCount] = array[i];
+                secondCount++;
+            }
+        }
         return newArray;
+
+
+
     }
 }
--- src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
@@ -8,14 +8,58 @@ public final class StringDuplicateDeleter extends DuplicateDeleter<String> {
     public StringDuplicateDeleter(String[] intArray) {
         super(intArray);
     }
+    public int getOccurances(String c){
+        int count = 0;
+        for(int i = 0; i < array.length; i++){
+            if(c.equals(array[i]))
+                count++;
+        }
+        return count;
+    }
 
     @Override
     public String[] removeDuplicates(int maxNumberOfDuplications) {
-        return new String[0];
+        int count = 0;
+        int secondCount = 0;
+        String[] tempArray = new String[0];
+
+        for(int i = 0; i < array.length; i++){
+            if(getOccurances(array[i]) < maxNumberOfDuplications){
+                tempArray = new String[tempArray.length + 1];
+                tempArray[count] = array[i];
+                count++;
+            }
+        }
+        String[] newArray = new String[tempArray.length];
+        for(int i = 0; i < array.length; i++){
+            if(getOccurances(array[i]) < maxNumberOfDuplications){
+                newArray[secondCount] = array[i];
+                secondCount++;
+            }
+        }
+        return newArray;
     }
 
     @Override
     public String[] removeDuplicatesExactly(int exactNumberOfDuplications) {
-        return new String[0];
+        int count = 0;
+        int secondCount = 0;
+        String[] tempArray = new String[0];
+
+        for(int i = 0; i < array.length; i++){
+            if(getOccurances(array[i]) != exactNumberOfDuplications){
+                tempArray = new String[tempArray.length + 1];
+                tempArray[count] = array[i];
+                count++;
+            }
+        }
+        String[] newArray = new String[tempArray.length];
+        for(int i = 0; i < array.length; i++){
+            if(getOccurances(array[i]) != exactNumberOfDuplications){
+                newArray[secondCount] = array[i];
+                secondCount++;
+            }
+        }
+        return newArray;
     }
 }
